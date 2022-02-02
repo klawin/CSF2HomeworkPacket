ceb118dc0b241b97fb9ff5317b4f09a3b9556eb2
Kayla Nguyen <kaylannguyen@outlook.com>
2022-02-01 20:37:06 -0600

    student

---
+++ CSF2HomeworkPacket/ClassesLibrary/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //FIELDS
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //PROPERTY
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+        //CONSTRUCTOR
+
+        //FQCTOR
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }
+
+        //Default/Unqualified CTOR
+        public Student()
+        {
+
+        }
+
+        //METHOD
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("\nStudent: {0}, {1}\n" +
+                "ID: {2}\n" +
+                "GPA: {3:n1}", LastName, FirstName, Id, Gpa);
+        }
+
+    }//end Student
+}//end namespace
--- CSF2HomeworkPacket/TesterProgram/Program.cs
+++ CSF2HomeworkPacket/TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,6 +11,23 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            Console.Title = "Tester Program";
+
+            Console.WriteLine("Tester Program");
+
+            Student s1 = new Student();
+            s1.FirstName = "Kayla";
+            s1.LastName = "Nguyen";
+            s1.Id = "12345";
+            s1.Gpa = 4.0f;
+
+            Console.WriteLine("\nStudent: {0}, {1}\n" +
+                "ID: {2}\n" +
+                "GPA: {3:n1}", s1.LastName, s1.FirstName, s1.Id, s1.Gpa);
+
+            Student s2 = new Student("Quentin", "Carter", "54321", 3.8f);
+            Console.WriteLine(s2);
+
+        }//end Main()
+    }//end class
+}//end namespace
